96d2dbb0085a431cd52a0b4f16b2839f655d017b
JureVidrih <jurevidrih@gmail.com>
2018-04-21 18:17:44 +0200

    Ive modified the routes and I added the ability to post new posts on the blog.

---
+++ assets/js/dbutils.js
@@ -0,0 +1,113 @@
+var mongoose = require('mongoose');
+
+var dbutils = {
+    connectToMongoServer: function connectToMongoServer() {
+        mongoose.connect("mongodb://localhost/simpleblogcms");
+        
+        this.createAModelFromSchema();
+    },
+    createAModelFromSchema: function createAModelFromSchema() {
+        this.userSchema = new mongoose.Schema({
+            username: String,
+            password: String,
+            about: String
+        });
+        this.userDBModel = mongoose.model("User", this.userSchema);
+
+        this.postSchema = mongoose.Schema({
+            postTitle: String,
+            postAuthor: String,
+            postContent: String,
+            originalDate: String
+        });
+
+        this.postDBModel = mongoose.model("Post", this.postSchema);
+    },
+    usersArray: [],
+    updateUsers: function updateUsers() {
+        this.userDBModel.find({}, function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("List of all users: ");
+                console.log(result);
+                this.usersArray = result;
+            }
+        }.bind(this));
+    },
+    addAUser: function addAUser(newUser) {
+        newUser = new this.userDBModel(newUser);
+        newUser.save(function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(result + " added to the database.");
+                this.updateUsers();
+            }
+        }.bind(this));
+    },
+    postsArray: [],
+    findAPostById: function findAPostById(id, errAction, successAction) {
+        this.postDBModel.findById(id, function(err, foundPost) {
+            if(err) {
+                errAction();
+            } else {
+                successAction(foundPost);
+            }
+        });
+    },
+    updatePosts: function updatePosts() {
+        this.postDBModel.find({}, function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("List of all posts: ");
+                console.log(result);
+                this.postsArray = result;
+            }
+        }.bind(this));
+    },
+    addAPost: function addAPost(newPost) {
+        newPost = new this.postDBModel(newPost);
+        newPost.save(function(err, result) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(result + " added to the database.");
+                this.updatePosts();
+            }
+        }.bind(this));
+    },
+    checkUserName: function checkUserName(username) {
+        console.log("Checking user name..");
+        success = true;
+        this.usersArray.forEach(function(userelem) {
+            if(userelem.username === username) {
+                success = false;
+
+                return;
+            }
+        });
+
+        return success;
+    },
+    authenticate: function authenticate(user) {
+        console.log("Authenticating..");
+        success = false;
+        this.usersArray.forEach(function(userelem) {
+            console.log(userelem.username + " | " + user.username);
+            if(userelem.username === user.username && userelem.password === user.password) {
+                success = true;
+    
+                return;
+            }
+        });
+    
+        return success;
+    },
+    addANewUser: function addANewUser(newUser) {
+
+    }
+};
+
+module.exports = dbutils;
--- server.js
+++ server.js
@@ -3,21 +3,20 @@ app = express();
 
 var bpars = require('body-parser');
 
+var dbutils = require('./assets/js/dbutils');
+dbutils.connectToMongoServer();
+dbutils.updateUsers();
+dbutils.updatePosts();
+
 app.use(express.static("public"));
 app.use(express.static("assets"));
 app.use(express.static("build"));
 app.use(bpars.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var posts = [
-    {title: "Man must explore, and this is exploration at its greatest", content: "Problems look mighty small from 150 miles up", author: "Jure", originalDate: "September 12, 2007"},
-    {title: "Title 2", content: "Some content.", author: "Jure", originalDate: "September 12, 2007"},
-    {title: "Title 3", content: "Some content.", author: "Jure", originalDate: "September 12, 2007"},
-    {title: "Title 4", content: "Some content.", author: "Jure", originalDate: "September 12, 2007"}
-];
-
 app.get("/", function(req, res) {
-    res.render("landing", {origin: "landing", posts: posts});
+    var posts = dbutils.postsArray;
+    res.render("index", {origin: "index", posts: posts});
 });
 
 app.get("/gallery", function(req, res) {
@@ -25,11 +24,36 @@ app.get("/gallery", function(req, res) {
 });
 
 app.get("/posts", function(req, res) {
+    var posts = dbutils.postsArray;
     res.render("partials/posts", {posts: posts});
 });
 
 app.get("/posts/new", function(req, res) {
-    res.render("formNewPost", {origin: "newPost"});
+    res.render("postNew", {origin: "newPost"});
+});
+
+app.get("/posts/show/:id", function(req, res) {
+    var postId = req.params.id;
+
+    this.errAction = function() {
+        res.send("Post with an id of " + postId + " wasn't found.");
+    }
+
+    this.successAction = function(post) {
+        res.render("postShow.ejs", {post: post});
+    }
+
+    var post = dbutils.findAPostById(postId, this.errAction, this.successAction);
+
+    // postId = Number(postId);
+
+    // if(isNaN(postId) || postId >= posts.length) {
+    //     console.log("An invalid post show route with an Id of " + req.params.id + " has been accessed.");
+    //     res.send("Invalid post ID! It most likely isn't a number!");
+    // } else {
+    //     console.log("Showing post #" + postId);
+    //     res.render("postShow.ejs", {post: posts[postId]});
+    // }
 });
 
 app.post("/posts", function(req, res) {
@@ -37,7 +61,7 @@ app.post("/posts", function(req, res) {
     var postAuthor = req.body.postAuthor;
     var postContent = req.body.postContent;
     var postDate = new Date();
-    posts.push({title: postTitle, content: postContent, author: postAuthor, originalDate: postDate});
+    dbutils.addAPost({postTitle: postTitle, postContent: postContent, postAuthor: postAuthor, originalDate: postDate});
     res.redirect("/");
 });
 
@@ -45,12 +69,36 @@ app.get("/login", function(req, res) {
     res.render("login");
 });
 
-app.get("/signup", function(req, res) {
-    res.render("login");
+app.post("/login", function(req, res) {
+    username = req.body.username;
+    password = req.body.password;
+
+    authSuccess = dbutils.authenticate({username: username, password: password});
+
+    if(authSuccess) {
+        console.log("User " + username + " has successfully logged in.");
+    } else {
+        console.log("An unsuccessful attempt at login has occured (username: " + username + ", password: " + password + ").");
+    }
+
+    res.redirect("/");
+});
+
+app.get("/signup/new", function(req, res) {
+    res.render("signupForm");
 });
 
 app.post("/signup", function(req, res) {
-    res.render("login");
+    newUser = {username: req.body.username, password: req.body.password, about: req.body.aboutuser};
+    authSuccess = dbutils.checkUserName(newUser.username);
+    if(authSuccess) {
+        dbutils.addAUser(newUser);
+        console.log("New user created! Data: " + newUser.username + ", " + newUser.password);
+        res.render("signupSuccess", {newUserData: newUser, id: dbutils.usersArray.length});
+    } else {
+        console.log("An unsuccessful attempt to register an account has occured.");
+        res.send("Invalid username. Try another one.")
+    }
 });
 
 app.listen(3000, "localhost", function() {
